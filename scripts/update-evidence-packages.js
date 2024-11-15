20a45bdc4cacb97e857c855d67f0b3a86e32d020
Zachary Stence <zachstence@gmail.com>
2024-11-15 11:02:21 -0600

    feat: delete node_modules and package-lock.json before installing in update script

--- scripts/update-evidence-packages.js
+++ scripts/update-evidence-packages.js
@@ -25,6 +25,14 @@ const evidencePackagesWithLatest = Object.keys(initialEvidencePackages)
   .join(" ");
 const updateCommand = `npm install ${evidencePackagesWithLatest}`;
 
+// Delete node_modules and package-lock.json
+process.stdout.write("Deleting node_modules and package-lock.json...");
+fs.rmSync("node_modules", { recursive: true, force: true });
+fs.rmSync("package-lock.json", { force: true });
+console.log(" Done");
+
+// Install
+console.log("Installing latest versions of evidence packages...");
 spawnSync(updateCommand, { shell: true, stdio: "inherit" });
 
 const finalEvidencePackages = readEvidencePackages();
